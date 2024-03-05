2621cdcddfbeb7bdc7ef87b60ac45b86c403cdb1
Adrian Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-05 17:46:46 +0330

    Delete CS50/CS50P directory

--- CS50/CS50P/PSET1/deep.py
+++
@@ -1,5 +0,0 @@
-answer = input("What is  the answer to the Great Question of Life, the Universe and Everything? ").lower()
-if "42" in answer or answer == "forty two" or answer == "forty-two":
-    print("Yes")
-else:
-    print("No")
--- CS50/CS50P/PSET2/camel.py
+++
@@ -1,9 +0,0 @@
-camelCase = input("camelCase: ")
-
-for ch in camelCase:
-    if ch.isupper():
-        camelCase = camelCase.replace(ch, f"_{ch.lower()}")
-
-print(f"snake_case: {camelCase}")
-
-    
